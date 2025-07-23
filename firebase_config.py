601ecaad760d7a95fd6d25f86319d8a8f3f423d9
Alithechosenknight <115343530+Alithechosenknight@users.noreply.github.com>
2025-07-23 12:17:34 +0300

    Update firebase_config.py trryyyyy

--- firebase_config.py
+++ firebase_config.py
@@ -3,9 +3,11 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import json
 
+# Initialize Firebase Admin SDK
 if not firebase_admin._apps:
     service_account_info = json.loads(st.secrets["FIREBASE_SERVICE_ACCOUNT"])
     cred = credentials.Certificate(service_account_info)
     firebase_admin.initialize_app(cred)
 
+# Export Firestore client
 db = firestore.client()
